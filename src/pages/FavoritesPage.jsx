09bcd2c2e8205ed21b4f1b87955468af56bc6184
AnnaM135 <68158093+AnnaM135@users.noreply.github.com>
2022-11-24 16:56:32 +0400

    feat: creation favorite page

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,5 +1,18 @@
+import { useAppSelector } from "../hooks/redux"
+
 export function FavoritesPage() {
+    const {favorites} = useAppSelector(state => state.github)
+
+    if(favorites.length === 0) return <p className="text.center">No items</p>
     return (
-        <div>sdajsdalsd</div>
+        <div className="flex justify-center pt-10 mx-auto h-screen">
+            <ul className="list-none">
+                {favorites.map(f => (
+                    <li key={f}>
+                        <a href={f} target="_blank">{f}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
